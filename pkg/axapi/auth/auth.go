55d442bd6b4ba0292b509c30bad5f84349b8df78
masanetes <masanetes@gmail.com>
2022-06-16 20:21:45 +0900

    impl authenticate

---
+++ pkg/axapi/auth/auth.go
@@ -0,0 +1,55 @@
+package auth
+
+import (
+	"encoding/json"
+	"github.com/masanetes/acos-client-go/utils"
+)
+
+type Request struct {
+	Credentials `json:"credentials"`
+}
+
+type Credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type Response struct {
+	*AuthResponse `json:"authresponse"`
+}
+
+type AuthResponse struct {
+	Signature   string `json:"signature"`
+	Description string `json:"description"`
+}
+
+type operator struct {
+	utils.HttpClient
+}
+
+type Operator interface {
+	Request(req *Request) (*AuthResponse, error)
+}
+
+func New(c utils.HttpClient) Operator {
+	return &operator{c}
+}
+
+func (o *operator) Request(req *Request) (*AuthResponse, error) {
+	body, err := json.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := o.POST("/auth", body)
+	if err != nil {
+		return nil, err
+	}
+
+	var response Response
+	if err = res.UnmarshalJson(&response); err != nil {
+		return nil, err
+	}
+
+	return response.AuthResponse, nil
+}
