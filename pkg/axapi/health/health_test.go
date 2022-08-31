13005abccc297e2b375996986c496c7cb2a8dcab
m-terra <andy.baer@gmail.com>
2022-08-31 11:19:38 +0200

    preserve http status in response

--- pkg/axapi/health/health_test.go
+++ pkg/axapi/health/health_test.go
@@ -1,6 +1,7 @@
 package health_test
 
 import (
+	"github.com/ureuzy/acos-client-go/pkg/axapi/errors"
 	"io"
 	"net/http"
 	"strings"
@@ -34,4 +35,30 @@ func TestGetMonitor(t *testing.T) {
 	res, err := sut.Montitor.Get("resource1")
 	Ω(err).ShouldNot(HaveOccurred())
 	Ω(res).ShouldNot(BeNil())
+}
+
+func TestNotFound(t *testing.T) {
+	RegisterTestingT(t)
+
+	mockCtrl := gomock.NewController(t)
+	httpc := mockutils.NewMockHTTPClient(mockCtrl)
+	sut := health.New(httpc)
+
+	body := io.NopCloser(strings.NewReader("{}"))
+
+	resp := &utils.Response{
+		Response: &http.Response{
+			Status:     "404 not found",
+			StatusCode: http.StatusNotFound,
+			Body:       body,
+		},
+	}
+
+	httpc.EXPECT().GET("health/monitor/resource1").Return(resp, nil)
+
+	_, err := sut.Montitor.Get("resource1")
+
+	Ω(err).Should(HaveOccurred())
+	errUnwrapped := err.(*errors.ResponseBody)
+	Ω(errUnwrapped.StatusCode).Should(BeIdenticalTo(http.StatusNotFound))
 }
