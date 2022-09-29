a0236bd1b6618613c5290c78c3812c49ed041b86
m-terra <andy.baer@gmail.com>
2022-09-29 16:28:15 +0200

    ip server does not support user-tag :(

--- pkg/axapi/gslb/site/siteipserver/types.go
+++ pkg/axapi/gslb/site/siteipserver/types.go
@@ -1,8 +1,5 @@
 package siteipserver
 
-import "github.com/ureuzy/acos-client-go/pkg/axapi/shared"
-
 type IPServer struct {
-	shared.AxaBase `json:",inline"`
-	IPServerName   string `json:"ip-server-name,omitempty"`
+	IPServerName string `json:"ip-server-name,omitempty"`
 }
